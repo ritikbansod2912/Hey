9f2c0a814038decc3ede1e438b646fa6d5f42f64
ritikbansod2912 <ritikbansod2912@gmail.com>
2023-08-06 10:42:29 +0530

    Added a mini calculator program

---
+++ calculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class calculator{
+	public static void main(String[] args){
+		System.out.println("Enter two number");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Enter the operation sign to perform (+,-,*,/)");
+		char c = sc.next().charAt(0);
+
+		switch(c){
+			case '+': System.out.println("Addition : "+ (a+b));
+				  break;
+			case '-': System.out.println("Subtration : "+(a-b));
+				  break;
+			case '*': System.out.println("Multiplication : "+(a*b));
+				  break;
+			case '/': System.out.println("Division : "+(a/b));
+				  break;
+		}
+	}
+}
+
